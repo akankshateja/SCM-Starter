0e338db7d7a08e517d3bf1b81c7aeb9cf6de6567
akankshateja <136678468+akankshateja@users.noreply.github.com>
2023-08-22 00:49:22 +0530

    create Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -3,59 +3,18 @@ pragma solidity ^0.8.9;
 
 //import "hardhat/console.sol";
 
-contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+contract SimpleContract {
+    address public owner;
+    string public name;
+    string public message;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    function setMessage(string memory _message) public {
+        message = _message;
+        owner = msg.sender;
+        name = "Akanksha";
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
-    }
-
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
-    }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+    function getMessage() public view returns (string memory) {
+        return message;
     }
 }
-
